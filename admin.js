e85fff04c47cc7e304d0d81410538c8f86fa5932
AmsiyatMonday <alyaaalareeqi@gmail.com>
2026-07-30 15:40:13 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,37 @@
+const saveBtn = document.getElementById("save");
 
+saveBtn.onclick = () => {
+
+    const title = document.getElementById("title").value;
+    const chapter = document.getElementById("chapter").value;
+    const pdf = document.getElementById("pdf").files[0];
+
+    if(title === ""){
+        alert("اكتبي عنوان الفصل");
+        return;
+    }
+
+    if(chapter === ""){
+        alert("اكتبي رقم الفصل");
+        return;
+    }
+
+    if(!pdf){
+        alert("اختاري ملف PDF");
+        return;
+    }
+
+    alert(
+`تم تجهيز الفصل.
+
+العنوان:
+${title}
+
+رقم الفصل:
+${chapter}
+
+الملف:
+${pdf.name}`
+    );
+
+};
