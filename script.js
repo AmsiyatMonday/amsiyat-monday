0a45385cae12ef9d884ce5a700c0dfe8bd1edc1e
AmsiyatMonday <alyaaalareeqi@gmail.com>
2026-07-30 03:58:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+// حفظ آخر صفحة
+let currentPage = localStorage.getItem("page") || 1;
 
+function savePage(page){
+    currentPage = page;
+    localStorage.setItem("page", page);
+}
+
+function nextPage(){
+    currentPage++;
+    savePage(currentPage);
+    loadPage();
+}
+
+function previousPage(){
+    if(currentPage > 1){
+        currentPage--;
+        savePage(currentPage);
+        loadPage();
+    }
+}
+
+function loadPage(){
+    const page = document.getElementById("page-number");
+    if(page){
+        page.textContent = currentPage;
+    }
+}
+
+// الوضع الليلي
+function toggleTheme(){
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        localStorage.setItem("theme","light");
+    }else{
+        localStorage.setItem("theme","dark");
+    }
+}
+
+window.onload = ()=>{
+    loadPage();
+
+    if(localStorage.getItem("theme")=="light"){
+        document.body.classList.add("light");
+    }
+};
